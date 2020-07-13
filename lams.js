b376b8617d7b5ab3f9d0b769000cfb5817e62591
uncreative_username <asd19901234@gmail.com>
2020-07-14 00:47:50 +0800

    userscript for lams video control

---
+++ lams.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         lams
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  change lams video speed and download video button
+// @author       You
+// @match        https://lams.ntu.edu.sg/lams/tool/lanb11/learning/learner.do*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    alert("working");
+})();
