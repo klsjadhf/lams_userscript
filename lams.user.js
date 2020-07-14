3eb2c83f8b62e86759234793deb203e7418c747d
uncreative_username <asd19901234@gmail.com>
2020-07-14 16:29:46 +0800

    working detect if video1 loaded

--- lams.user.js
+++ lams.user.js
@@ -13,23 +13,45 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    'use strict'
 
     console.log("tampermonkey script running");
 
 
     var observer = new MutationObserver(function(){
         console.log("DOM changed");
+        
+        var linkDisplayed = 0;
 
+        //remove annoying box
         if(document.querySelector("#div_index") !== null){
-            console.log(document.querySelector("#div_index").style.width);
+            // console.log(document.querySelector("#div_index").style.width);
             if(document.querySelector("#div_index").style.width === "100%"){
                 document.querySelector("#div_index").style.width = "0px";
             }
         }
+
+        //video
+        if(document.querySelector("#Video1_html5_api") !== null){
+            document.querySelector("#Video1_html5_api").addEventListener("canplay",video1Onload);
+
+            //console.log(document.querySelector("#Video1_html5_api").src);
+            if(document.querySelector("#Video1_html5_api").src.length !== 0){
+                //console.log("video1 src: " + document.querySelector("#Video1_html5_api").src);
+            }
+            //alert(document.querySelector("#Video1_html5_api").src);
+        }
     });
     observer.observe(document, {childList: true, subtree: true});
+
+    function video1Onload(){
+        console.log("video1 can play");
+        if(document.querySelector("#Video1_html5_api").src.length !== 0){
+            console.log("video1 src: " + document.querySelector("#Video1_html5_api").src);
+        }
+    }
 })();
+
 /*
 localStorage.getItem('arvplayer-playbackRate');
 localStorage.setItem('arvplayer-playbackRate',i+1);
