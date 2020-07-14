9088dc066fbfca42a744d415326987bb1c6f8507
uncreative_username <asd19901234@gmail.com>
2020-07-14 15:03:37 +0800

    change update url

--- lams.user.js
+++ lams.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         lams
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.21
 // @description  change lams video speed and download video button
 // @author       klsjadhf
 // @homepage     https://github.com/klsjadhf/lams_userscript
-// @updateURL    https://github.com/klsjadhf/lams_userscript/blob/master/lams.user.js
-// @downloadURL  https://github.com/klsjadhf/lams_userscript/blob/master/lams.user.js
+// @updateURL    https://github.com/klsjadhf/lams_userscript/raw/master/lams.user.js
+// @downloadURL  https://github.com/klsjadhf/lams_userscript/raw/master/lams.user.js
 // @match        http*://presentur.ntu.edu.sg/aculearn-idm/v8/studio/embed.asp*
 // @grant        none
 // @run-at document-idle
