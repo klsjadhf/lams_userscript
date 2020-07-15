bbbde860d928b439c3ca598989a4730e970ffd71
uncreative_username <asd19901234@gmail.com>
2020-07-16 04:30:38 +0800

    press and hold for some buttons

--- lams.user.js
+++ lams.user.js
@@ -25,6 +25,8 @@
     console.log(GM_listValues().map(GM_getValue));
     console.log("tampermonkey script running on " + window.location.hostname);
 
+    var videoOnLoadAdded = false;
+
     if(document.URL.match(/https:\/\/lams\.ntu\.edu\.sg\/lams\/tool\/lanb11\/learning\/learner\.do/)){
         //if user pressed key in wrong window, send to iframe
         document.addEventListener("keydown", (keydownEvent) =>{
@@ -73,7 +75,10 @@
 
             //video
             if(document.querySelector("#Video1_html5_api") !== null){
-                document.querySelector("#Video1_html5_api").addEventListener("canplay",video1Onload);
+                if (videoOnLoadAdded === false){
+                    videoOnLoadAdded = true;
+                    document.querySelector("#Video1_html5_api").addEventListener("canplay", video1Onload);
+                }
             }
         }
     });
@@ -155,78 +160,105 @@
         document.addEventListener("keydown", (keydownEvent)=>{
             onKeypress(getPressedkey(keydownEvent));
         });
+
+        document.querySelector("#Video1_html5_api").removeEventListener("canplay", video1Onload);
     }
 
     function onKeypress(keyInfo){
         var videoElem = document.querySelector("#Video1_html5_api");
+        var newTime;
 
         console.log("pressed " + keyInfo.pressedKey);
 
-        //play/pause
-        if(keyInfo.pressedKey === "p" || keyInfo.pressedKey === "P"){
-            document.querySelector(".vjs-play-control").click();
-            console.log("play/pause video");
+        if(!keyInfo.repeat){ //keys that should not press and hold
+            //play/pause
+            if(keyInfo.pressedKey === "p" || keyInfo.pressedKey === "P"){
+                document.querySelector(".vjs-play-control").click();
+                console.log("play/pause video");
+            }
+            //slow down
+            else if(keyInfo.pressedKey === ","){
+                arvplayer.playbackRate(fracPlusSub("-", arvplayer.playbackRate(), 0.5));
+                console.log("slow coarse " + arvplayer.playbackRate());
+            }
+            //speed up
+            else if(keyInfo.pressedKey === "."){
+                arvplayer.playbackRate(fracPlusSub("+", arvplayer.playbackRate(), 0.5));
+                console.log("fast coarse " + arvplayer.playbackRate());
+            }
+            //set saved playback speed
+            else if(keyInfo.pressedKey === "s" || keyInfo.pressedKey === "S"){
+                // console.log(videojs.getPlayers());
+                arvplayer.playbackRate(2);
+                // videoElem.playbackRate = 2;
+                console.log("custom speed " + arvplayer.playbackRate());
+            }
+            //mute/unmute
+            else if(keyInfo.pressedKey === "m" || keyInfo.pressedKey === "M"){
+                arvplayer.muted(!arvplayer.muted());
+                // document.querySelector(".vjs-mute-control").click();
+                console.log("mute " + arvplayer.muted());
+            }
+            //toggle fullscreen
+            else if(keyInfo.pressedKey === "f" || keyInfo.pressedKey === "F"){
+                // console.log(IsFullScreen());
+                // arvplayer.isFullscreen(!arvplayer.isFullscreen());
+                if(IsFullScreen()) arvplayer.exitFullscreen();
+                else document.querySelector(".arv_fullscreenButton").click();
+                console.log("fullscreen");
+            }
         }
-        //slow down
-        else if(keyInfo.pressedKey === ","){
-            arvplayer.playbackRate(fracPlusSub("-", arvplayer.playbackRate(), 0.5));
-            console.log("slow coarse");
-        }
+        //allowed press and hold
         //slow down fine
-        else if(keyInfo.pressedKey === "<"){
+        if(keyInfo.pressedKey === "<"){
             arvplayer.playbackRate(fracPlusSub("-", arvplayer.playbackRate(), 0.1));
-            console.log("slow fine");
-        }
-        //speed up
-        else if(keyInfo.pressedKey === "."){
-            arvplayer.playbackRate(fracPlusSub("+", arvplayer.playbackRate(), 0.5));
-            console.log("fast coarse");
+            console.log("slow fine " + arvplayer.playbackRate());
         }
         //speed up fine
         else if(keyInfo.pressedKey === ">"){
             arvplayer.playbackRate(fracPlusSub("+", arvplayer.playbackRate(), 0.1));
-            console.log("fast fine");
-        }
-        //set saved playback speed
-        else if(keyInfo.pressedKey === "s" || keyInfo.pressedKey === "S"){
-            // console.log(videojs.getPlayers());
-            arvplayer.playbackRate(2);
-            // videoElem.playbackRate = 2;
-            console.log("custom speed");
+            console.log("fast fine " + arvplayer.playbackRate());
         }
         //rewind
         else if(keyInfo.pressedKey === "ArrowLeft"){
-            console.log("rewind");
+            // console.log("rewind prev " + videoElem.currentTime);
+            // newTime = fracPlusSub("-", videoElem.currentTime, 10)
+            // if(newTime <= 0) videoElem.currentTime = 0;
+            // else videoElem.currentTime = newTime;
+            // console.log("rewind " + videoElem.currentTime);
+            newTime = fracPlusSub("-", arvplayer.currentTime(), 3)
+            if(newTime <= 0) arvplayer.currentTime(0);
+            else arvplayer.currentTime(newTime);
+            console.log("rewind " + arvplayer.currentTime());
         }
         //foward
         else if(keyInfo.pressedKey === "ArrowRight"){
-            console.log("foward");
+            // console.log("foward prev " + videoElem.currentTime);
+            // newTime = fracPlusSub("+", videoElem.currentTime, 10)
+            // if(newTime >= videoElem.duration) videoElem.currentTime = videoElem.duration;
+            // else videoElem.currentTime = newTime;
+            // console.log("foward " + videoElem.currentTime);
+            newTime = fracPlusSub("+", arvplayer.currentTime(), 3)
+            if(newTime >= arvplayer.duration()) arvplayer.currentTime(arvplayer.duration());
+            else arvplayer.currentTime(newTime);
+            console.log("foward " + arvplayer.currentTime());
         }
         //volume up
         else if(keyInfo.pressedKey === "ArrowUp"){
-            console.log("volume up");
+            arvplayer.volume(fracPlusSub("+", parseFloat(arvplayer.volume()), 0.05));
+            console.log("volume up " + arvplayer.volume());
         }
         //volume down
         else if(keyInfo.pressedKey === "ArrowDown"){
-            console.log("volume down");
-        }
-        //mute/unmute
-        else if(keyInfo.pressedKey === "m" || keyInfo.pressedKey === "M"){
-            document.querySelector(".vjs-mute-control").click();
-            console.log("mute");
-        }
-        //toggle fullscreen
-        else if(keyInfo.pressedKey === "f" || keyInfo.pressedKey === "F"){
-            // console.log(IsFullScreen());
-            if(IsFullScreen()) arvplayer.exitFullscreen();
-            else document.querySelector(".arv_fullscreenButton").click();
-            console.log("fullscreen");
+            arvplayer.volume(fracPlusSub("-", parseFloat(arvplayer.volume()), 0.05));
+            console.log("volume down " + arvplayer.volume());
         }
     }
 
     function getPressedkey(keydownEvent){
         var keyInfo = {
             pressedKey: keydownEvent.key,
+            repeat: keydownEvent.repeat,
             modifier: ""
         }
 
@@ -244,10 +276,10 @@
     //more accurate addition and subtraction of floating point 
     function fracPlusSub(sign, val1, val2){
         if(sign === "+"){
-            return (val1*10 + val2*10)/10;
+            return ((val1*100 + val2*100)/100).toFixed(2);
         }
         else if(sign === "-"){
-            return (val1*10 - val2*10)/10;
+            return ((val1*100 - val2*100)/100).toFixed(2);
         }
         else return 0.0;
     }
